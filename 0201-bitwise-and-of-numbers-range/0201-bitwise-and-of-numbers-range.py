979dc0dfd94f88d76a4e91ab5aea4b1c3df4c255
yohanse mehabaw <mehabawyohanse793@gmail.com>
2024-07-08 17:09:56 +0300

    Time: 58 ms (27.45%), Space: 16.5 MB (31.08%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -2,7 +2,8 @@ class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
         ans = 0
         for i in range(32):
-            if (left & 2**i) and (right & 2**i) and right - left < 2**i:
-                ans += 2**i
+            value = 1 << i
+            if (left & value) and (right & value) and right - left < value:
+                ans += value
         return ans
         

2cf05a0a993e37bc65af7f920f60175b75372ac1
yohanse mehabaw <mehabawyohanse793@gmail.com>
2024-07-09 22:46:33 +0300

    Time: 210 ms (66.98%), Space: 20.5 MB (39.77%) - LeetHub

--- 2001-jump-game-vii/2001-jump-game-vii.py
+++ 2001-jump-game-vii/2001-jump-game-vii.py
@@ -1,12 +1,12 @@
 class Solution:
     def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
-        heap = [0]
+        queue = deque([0])
         n = len(s)
 
         for i in range(1, n):
-            while heap and i > heap[0] + maxJump:
-                heappop(heap)
+            while queue and i > queue[0] + maxJump:
+                queue.popleft()
 
-            if s[i] == "0" and heap and heap[0] + minJump <= i <= heap[0] + maxJump:
-                heappush(heap, i)
-        return (n - 1 ) in heap
+            if s[i] == "0" and queue and queue[0] + minJump <= i <= queue[0] + maxJump:
+                queue.append(i)
+        return (n - 1 ) in queue

53d3d3890a4c431bc0b1f5117375418631f507ba
yohanse mehabaw <mehabawyohanse793@gmail.com>
2024-08-13 12:32:25 +0300

    Time: 290 ms (39.13%), Space: 33 MB (41.78%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,32 +1,18 @@
-class Solution:
-    def change(self, amount: int, coins: List[int]) -> int:
-        @cache
-        def rec(amount, index):
-            
 
-            if index == len(coins):
-                return 1 if amount == 0 else 0
-
-            result = 0
-            for k in range(amount // coins[index] + 1):
-                result += rec(amount - k*coins[index], index + 1)
-            return result
-
-        return rec(amount, 0)
             
         
 
 
-# class Solution:
-#     def change(self, amount: int, coins: List[int]) -> int:
-#         n = len(coins)
-#         dp = [[0 for i in range(n + 1)] for j in range(amount + 1)]
-#         dp[0][0] = 1
-#         for money in range(1, amount + 1):
-#             for coin in range(n):
-#                 for k in range(money//coins[coin] + 1):
-#                     print(money, k, coins[coin], dp[money - k*coins[coin]][coin])
-#                     dp[money][coin + 1] += dp[money - k*coins[coin]][coin]
-               
-#         print(dp)
-#         return dp[-1][-1] 
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        dp = [[0 for i in range(n)] for j in range(amount + 1)]
+        dp[0] = [1 for i in range(n)]
+
+        for money in range(1, amount + 1):
+            for index in range(n):
+                dp[money][index] = dp[money][index - 1]
+                if money - coins[index] >= 0:
+                    dp[money][index] += dp[money - coins[index]][index]
+       
+        return dp[-1][-1] 

6166658df920599791d9081203b280d0c214b700
yohanse mehabaw <mehabawyohanse793@gmail.com>
2024-08-13 11:11:21 +0300

    Time: 1857 ms (6.63%), Space: 189.8 MB (6.12%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -2,11 +2,10 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         @cache
         def rec(amount, index):
-            if amount == 0:
-                return 1
+            
 
             if index == len(coins):
-                return 0
+                return 1 if amount == 0 else 0
 
             result = 0
             for k in range(amount // coins[index] + 1):
@@ -16,3 +15,18 @@ class Solution:
         return rec(amount, 0)
             
         
+
+
+# class Solution:
+#     def change(self, amount: int, coins: List[int]) -> int:
+#         n = len(coins)
+#         dp = [[0 for i in range(n + 1)] for j in range(amount + 1)]
+#         dp[0][0] = 1
+#         for money in range(1, amount + 1):
+#             for coin in range(n):
+#                 for k in range(money//coins[coin] + 1):
+#                     print(money, k, coins[coin], dp[money - k*coins[coin]][coin])
+#                     dp[money][coin + 1] += dp[money - k*coins[coin]][coin]
+               
+#         print(dp)
+#         return dp[-1][-1] 

e0c1fcbbd65b7df9c7293e3f1a42047f920efe4b
yohanse mehabaw <mehabawyohanse793@gmail.com>
2024-09-03 18:54:17 +0300

    Time: 68 ms (56.68%), Space: 17 MB (28.47%) - LeetHub

--- 0779-max-chunks-to-make-sorted-ii/0779-max-chunks-to-make-sorted-ii.py
+++ 0779-max-chunks-to-make-sorted-ii/0779-max-chunks-to-make-sorted-ii.py
@@ -1,17 +1,19 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
+        length = len(arr)
+
         index = [i for i in range(len(arr))]
         index.sort(key=lambda x:arr[x])
 
-        visited1 = {}
-        visited2 = {}
+        visited = {}
+        paritions = pointer = 0
 
-        paritions = 0
-        for i in range(len(arr)):
-            visited1[arr[i]] = visited1.get(arr[i], 0) + 1
-            visited2[arr[index[i]]] = visited2.get(arr[index[i]], 0) + 1
-
-            if visited1 == visited2:
-                paritions += 1
+        for sort_pointer in range(length):
+            visited[arr[index[sort_pointer]]] = visited.get(arr[index[sort_pointer]], 0) + 1
+            while pointer < length and visited.get(arr[pointer], 0):
+                visited[arr[pointer]] -= 1
+                pointer += 1
+           
+            paritions += (sort_pointer + 1 == pointer)
         return paritions
         

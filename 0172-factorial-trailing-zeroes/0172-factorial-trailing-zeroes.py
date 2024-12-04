1ecd6228c814615590e035b5ec71de95b6ceed08
yohanse mehabaw <mehabawyohanse793@gmail.com>
2024-12-05 01:21:04 +0300

    Time: 373 ms (18.32%), Space: 18.2 MB (7.04%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -2,14 +2,14 @@ class Solution:
     def trailingZeroes(self, n: int) -> int:
         
         two = five = 0
+        dp = [[0, 0] for i in range(n + 1)]
         for i in range(1, n + 1):
-            num = i
-            while num % 2 == 0:
-                two += 1
-                num //= 2
             
-            num = i
-            while num % 5 == 0:
-                five += 1
-                num //= 5
+            if i % 2 == 0:
+                dp[i][0] = dp[i//2][0] + 1
+                two += dp[i][0]
+                
+            if i % 5 == 0:
+                dp[i][1] = dp[i//5][1] + 1
+                five += dp[i][1]
         return min(two, five)

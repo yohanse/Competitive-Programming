341aa304bb77fca542e40e78f68de19efbcd571c
yohanse mehabaw <mehabawyohanse793@gmail.com>
2024-12-09 12:35:09 +0300

    Time: 696 ms (5.5%), Space: 42 MB (31.29%) - LeetHub

--- 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.py
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.py
@@ -3,16 +3,20 @@ class Solution:
         rows, cols = len(points), len(points[0])
 
         for row in range(1, rows):
-            left_to_right = [-inf for i in range(cols + 1)]
-            right_to_left = [-inf for i in range(cols + 1)]
+            left_to_right = [-inf for i in range(cols)]
+            right_to_left = [-inf for i in range(cols)]
 
+            prev = -inf
             for col in range(cols):
-                left_to_right[col + 1] = max(left_to_right[col], points[row - 1][col] + col)
+                left_to_right[col] = max(prev, points[row - 1][col] + col)
+                prev = max(prev, left_to_right[col])
 
+            prev = -inf
             for col in range(cols - 1, -1, -1):
-                right_to_left[col] = max(right_to_left[col + 1], points[row - 1][col] - col)
-            
+                right_to_left[col] = max(prev, points[row - 1][col] - col)
+                prev = max(prev, right_to_left[col])
+
             for col in range(cols):
-                points[row][col] += max(left_to_right[col + 1] - col, right_to_left[col] + col)
+                points[row][col] += max(left_to_right[col] - col, right_to_left[col] + col)
        
         return max(points[-1])

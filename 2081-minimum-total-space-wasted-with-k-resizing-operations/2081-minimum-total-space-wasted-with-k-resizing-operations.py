b0c5957690124bbc3460ccd90019c0d2e030a266
yohanse mehabaw <mehabawyohanse793@gmail.com>
2024-12-20 10:45:54 +0300

    Time: 6573 ms (5.17%), Space: 18.8 MB (56.9%) - LeetHub

--- 2081-minimum-total-space-wasted-with-k-resizing-operations/2081-minimum-total-space-wasted-with-k-resizing-operations.py
+++ 2081-minimum-total-space-wasted-with-k-resizing-operations/2081-minimum-total-space-wasted-with-k-resizing-operations.py
@@ -1,17 +1,17 @@
 class Solution:
-    def minSpaceWastedKResizing(self, nums: List[int], m: int) -> int:
+    def minSpaceWastedKResizing(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        dp = [[inf for i in range(m + 2)] for j in range(n + 1)]
+        dp = [[inf for i in range(k + 2)] for j in range(n + 1)]
         dp[0][0] = 0
 
         for i in range(n):
-            for j in range(m + 1):
+            for j in range(k + 1):
                 maxi = sumi = count = 0
-                for k in range(i, -1, -1):
+                for m in range(i, -1, -1):
 
-                    maxi = max(maxi, nums[k])
-                    sumi += nums[k]
+                    maxi = max(maxi, nums[m])
+                    sumi += nums[m]
                     count += 1
-                    
-                    dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[k][j] + maxi*count - sumi)
+
+                    dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[m][j] + maxi*count - sumi)
         return dp[-1][-1]

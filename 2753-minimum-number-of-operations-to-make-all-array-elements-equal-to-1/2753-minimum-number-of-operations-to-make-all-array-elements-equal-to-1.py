48df33de5fdb8fe8d2050d3250baeb3cd779ef83
yohanse mehabaw <mehabawyohanse793@gmail.com>
2025-01-02 16:26:50 +0300

    Time: 3 ms (52.78%), Space: 17.5 MB (25%) - LeetHub

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.py
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.py
@@ -2,12 +2,15 @@ class Solution:
     def minOperations(self, nums: List[int]) -> int:
         if 1 in nums:
             return len(nums) - nums.count(1)
-            
+
         smallest_leng = len(nums) + 1
         for i in range(len(nums)):
-            for j in range(i):
-                if gcd(*nums[j:i+1]) == 1:
-                    smallest_leng = min(smallest_leng, i - j + 1)
+            gcd_cal = nums[i]
+            for j in range(i, len(nums)):
+                gcd_cal = gcd(gcd_cal, nums[j])
+                if gcd_cal == 1:
+                    smallest_leng = min(smallest_leng, j - i + 1)
+                    break
        
         if smallest_leng == len(nums) + 1:
             return -1

c08e10f46266a0baff902b3af68de1ab0c607d60
yohanse mehabaw <mehabawyohanse793@gmail.com>
2025-01-09 09:33:45 +0300

    Time: 364 ms (58.29%), Space: 18.1 MB (47.91%) - LeetHub

--- 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.py
+++ 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.py
@@ -5,11 +5,7 @@ class Solution:
 
         for i in s:
             if i == "1":
-                one[0] += 1
-                one[1] += zero[0]
-                one[2] += zero[1]
+                one = [one[0] + 1, one[1] + zero[0], one[2] + zero[1]]
             else:
-                zero[0] += 1
-                zero[1] += one[0]
-                zero[2] += one[1]
+                zero = [zero[0] + 1, zero[1] + one[0], zero[2] + one[1]]
         return one[2] + zero[2]

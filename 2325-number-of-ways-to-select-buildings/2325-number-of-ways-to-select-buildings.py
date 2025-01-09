c3258558106d140df39dfdc46922d313e73a8c7a
yohanse mehabaw <mehabawyohanse793@gmail.com>
2025-01-09 09:32:09 +0300

    Time: 420 ms (54.94%), Space: 18.6 MB (30.32%) - LeetHub

---
+++ 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfWays(self, s: str) -> int:
+        zero = [0, 0, 0]
+        one = [0, 0, 0]
+
+        for i in s:
+            if i == "1":
+                one[0] += 1
+                one[1] += zero[0]
+                one[2] += zero[1]
+            else:
+                zero[0] += 1
+                zero[1] += one[0]
+                zero[2] += one[1]
+        return one[2] + zero[2]

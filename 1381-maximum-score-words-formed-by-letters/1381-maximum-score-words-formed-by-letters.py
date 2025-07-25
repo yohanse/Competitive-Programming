f973ec93370f8e76e19ec54da502cc83c6a0383c
Yohanse Mehabaw <mehabawyohanse793@gmail.com>
2025-07-25 16:25:37 +0300

    Time: 3 ms (100%), Space: 17.8 MB (91.87%) - LeetHub

--- 1381-maximum-score-words-formed-by-letters/1381-maximum-score-words-formed-by-letters.py
+++ 1381-maximum-score-words-formed-by-letters/1381-maximum-score-words-formed-by-letters.py
@@ -1,29 +1,29 @@
 class Solution:
     def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
-        count = [0] * 26
-        for i in letters:
-            count[ord(i) - ord("a")] += 1
+        # Let me start with a bruteforce solution we generate all the subsequences of wrods list and check if we creat that subsequence with in our letter if so we can calulate the score and we will do this for every subsequence and find out the amximum score 
 
-        res, N = [0], len(words)
+        # when we analyze the time complexity
+        # genrating subsequence is 2**n n is len(words)
+        # to check valid it won't takes us O(26) because 26 letter only and we can calculate the score while we are checking
+        # function cost will be m, m is each word length
 
-        def back(index, count, ans):
-            if index == N:
-                res[0] = max(res[0], ans)
-                return 
+        # so final time complexity is 26*2**n*m which will pass
 
-            temp = count.copy()
-            temp_ans = 0
+        # to optimize it we an add pruning by just stoping the resurion whenever if it is unvalid
+
+        possible_letters = Counter(letters)
+        def recursion(letters, index):
+            if index == len(words):
+                return 0
+            
+            # choose
+            copy_letter = letters.copy()
+            cost = choose_ans = 0
             for letter in words[index]:
-                i = ord(letter) - ord("a")
-                count[i] -= 1
-                if count[i] == -1:
-                    break
-                temp_ans += score[i]
+                letters[letter] = letters.get(letter, 0) + 1
+                cost += score[ord(letter) - ord('a')]
+                if letters[letter] > possible_letters.get(letter, 0):
+                    return recursion(copy_letter, index + 1)
             else:
-                back(index + 1, count, ans + temp_ans)
-            count = temp
-
-            back(index + 1, count, ans)
-
-        back(0, count, 0)
-        return res[0]
+                return max(recursion(letters, index + 1) + cost, recursion(copy_letter, index + 1))
+        return recursion({}, 0)

dc388da8e2835549ca8379a7f06230c84ef4eba0
Yohanse Mehabaw <mehabawyohanse793@gmail.com>
2025-07-30 12:30:56 +0300

    Time: 2298 ms (5%), Space: 25.6 MB (5.12%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,30 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+
+        cols = 1001
+        zigzag = [["" for _ in range(cols)] for _ in range(numRows)]
+
+        index = 0
+        col = 0
+        while index < len(s):
+            for i in range(numRows):
+                if index < len(s):
+                    zigzag[i][col] = s[index]
+                    index += 1
+            col += 1
+            for i in range(numRows - 2, 0, -1):
+                if index < len(s):
+                    zigzag[i][col] = s[index]
+                    index += 1
+            col += 1
+        
+        result = []
+        for i in range(numRows):
+            for j in range(cols):
+                if zigzag[i][j] != "":
+                    result.append(zigzag[i][j])
+        
+        return "".join(result)
+        

73833086eb25fa7ef9a3ce2a38704779a31f010a
Yohanse Mehabaw <mehabawyohanse793@gmail.com>
2025-08-07 23:43:39 +0300

    Time: 0 ms (100%), Space: 17.8 MB (38.77%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -4,15 +4,12 @@ class Solution:
         x1, y1 = (0, 0)
         x2, y2 = (rows - 1, cols - 1)
         result = []
-        while x1 <= x2 and y1 <= y2:
+        while x1 < x2 and y1 < y2:
             for i in range(y1, y2 + 1):
                 result.append(matrix[x1][i])
             
             for i in range(x1 + 1, x2 + 1):
                 result.append(matrix[i][y2])
-
-            if x1 == x2 or y2 == y1:
-                return result
 
             for i in range(y2 - 1, y1 - 1, -1):
                 result.append(matrix[x2][i])
@@ -22,6 +19,14 @@ class Solution:
             
             x1, y1 = x1 + 1, y1 + 1
             x2, y2 = x2 - 1, y2 - 1
+        
+        if x1 == x2:
+            for i in range(y1, y2 + 1):
+                result.append(matrix[x1][i])
+        
+        elif y1 == y2:
+            for i in range(x1, x2 + 1):
+                result.append(matrix[i][y1])
         return result
 
 

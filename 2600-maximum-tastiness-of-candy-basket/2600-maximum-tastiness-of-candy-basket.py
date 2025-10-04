c67f6ebe5b7723ecb1579ba517902cae36caa8ea
Yohanse Mehabaw <mehabawyohanse793@gmail.com>
2025-10-04 20:19:11 +0300

    Time: 599 ms (48.71%), Space: 29 MB (95.47%) - LeetHub

--- 2600-maximum-tastiness-of-candy-basket/2600-maximum-tastiness-of-candy-basket.py
+++ 2600-maximum-tastiness-of-candy-basket/2600-maximum-tastiness-of-candy-basket.py
@@ -1,19 +1,21 @@
 class Solution:
-    def good_function(self, price, testiness, bucket_size):
-        prev = price[0]
-        curr_bucket_size = 1
-        for i in range(len(price)):
-            if price[i] - prev >= testiness:
-                prev = price[i]
-                curr_bucket_size += 1
-        return curr_bucket_size >= bucket_size
+    def is_valid(self, t, prices, k):
+        count = 0
+        prev = -inf
 
+        for i in range(len(prices)):
+            if prices[i] - prev >= t:
+                count += 1
+                prev = prices[i]
+        
+        return count >= k
+            
     def maximumTastiness(self, price: List[int], k: int) -> int:
         price.sort()
         l, r = 0, max(price) - min(price)
-        while l < r:
+        while r > l:
             mid = (l + r + 1) // 2
-            if self.good_function(price, mid, k):
+            if self.is_valid(mid, price, k):
                 l = mid
             else:
                 r = mid - 1

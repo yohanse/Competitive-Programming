11c69904d9cad7399120d19df2d8bcddec8d2c2d
Yohanse Mehabaw <mehabawyohanse793@gmail.com>
2025-10-16 10:08:57 +0300

    Time: 148 ms (59.05%), Space: 39.3 MB (24.3%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.py
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.py
@@ -1,30 +1,16 @@
 class Solution:
     def findSmallestInteger(self, nums: List[int], value: int) -> int:
+        remainders = {}
+        for num in nums:
+            remainder = num%value
+            remainders[remainder] = remainders.get(remainder, 0) + 1
+        
+        for i in range(len(nums) + 1):
+            remainder = i%value
+            if remainders.get(remainder, 0) == 0:
+                return i
+            
+            remainders[remainder] -= 1
         
         
-
-
-        def good_function(mid, nums):
-            length_nums = len(nums)
-            frequency_reminder = {}
-            for i in range(length_nums):
-                reminder = nums[i] % value
-                frequency_reminder[reminder] = frequency_reminder.get(reminder, 0) + 1
-
-            for i in range(mid):
-                reminder = i % value
-                if not frequency_reminder.get(reminder, 0):
-                    return False
-                frequency_reminder[reminder] -= 1
-            return True
-
-
-        l, r = 0, len(nums)
-        while l < r:
-            mid = (l + r + 1) // 2
-            if good_function(mid, nums.copy()):
-                l = mid
-            else:
-                r = mid - 1
-        return l
         
